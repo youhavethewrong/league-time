7327b52c6a2a1a1a2c1ba1941234c431340d16da
ESC <youhavethewrong@gmail.com>
2017-08-04 14:59:21 -0500

    Output all matches for the current season of a league.  Refactoring required, but it seems to work.

--- src/league_time/core.cljs
+++ src/league_time/core.cljs
@@ -10,56 +10,7 @@
 (defonce https (node/require "https"))
 (defonce fs (node/require "fs"))
 
-(def leagues
-  {"NA" 2
-   "EU" 3
-   "LCK" 5
-   "LMS" 6
-   "LPL" 7})
-
-(defn get-league
-  [league]
-  (if (contains? (set (keys leagues)) league)
-    (str "http://api.lolesports.com/api/v1/scheduleItems?leagueId=" (leagues league))
-    (println (str "No league " league " in " (keys leagues) "."))))
-
 (def useragent "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1")
-
-(defn extract-events
-  [data start-date end-date]
-  (let [parsed-start (.parse js/Date start-date)
-        parsed-end (.parse js/Date end-date)]
-    (->> data
-         (filter
-          (fn item-in-date-range [{time :scheduledTime :as item}]
-            (let [parsed-date (.parse js/Date time)]
-              (and (<= parsed-date parsed-end)
-                   (>= parsed-date parsed-start)))))
-         (map
-          (fn format-event [{time :scheduledTime
-                             {:keys [blockLabel blockPrefix subBlockLabel subBlockPrefix]} :tags}]
-            {:time time
-             :event (str blockPrefix (when-not (blank? blockPrefix) " ")
-                         blockLabel (when-not (blank? blockLabel) " ")
-                         subBlockPrefix (when-not (blank? subBlockPrefix) " ")
-                         subBlockLabel )})))))
-
-(defn time-<
-  [{time-string-a :time} {time-string-b :time}]
-  (let [parsed-a (.parse js/Date time-string-a)
-        parsed-b (.parse js/Date time-string-b)]
-    (compare parsed-a parsed-b)))
-
-(defn upcoming-matches
-  "Digs out the season, start date, and end date from a response to a
-   GET on the lolesports/scheduleItems API endpoint."
-  [data]
-  (let [tournies (filter #(:published %) (:highlanderTournaments data))
-        {:keys [description startDate endDate] :as current-tourney} (last tournies)
-        events (extract-events (:scheduleItems data) startDate endDate)
-        sorted-events (into (sorted-set-by time-<) events)]
-    (println (str "The current season is " description ".  It begins " startDate " and ends " endDate "."))
-    (pprint/pprint sorted-events)))
 
 (defn write-data
   [data filename]
@@ -70,11 +21,11 @@
 (defn parse-json
   [response]
   (let [data (js->clj (.parse js/JSON response) :keywordize-keys true)]
-;;    (write-data data "out.edn")
+    ;;    (write-data data "out.edn")
     data))
 
-(defn parse-response
-  [url]
+(defn interpret-data
+  [url f]
   (-> (js/Promise.
        (fn [resolve reject]
          (let [scheme (if (starts-with? url "https") https http)]
@@ -88,22 +39,92 @@
                        (-> res
                            (.on "data" #(swap! body str (.toString %)))
                            (.on "end" (fn [_] (resolve @body)))))))))))
-      (.then #(upcoming-matches (parse-json %)))
+      (.then f)
       (.catch js/console.error)))
+
+(defn time-<
+  [{time-string-a :time} {time-string-b :time}]
+  (let [parsed-a (.parse js/Date time-string-a)
+        parsed-b (.parse js/Date time-string-b)]
+    (compare parsed-a parsed-b)))
+
+(def leagues
+  {"NA" 2
+   "EU" 3
+   "NA-CS" 4
+   "EU-CS" 5
+   "LCK" 6
+   "LMS" 7
+   "LPL" 8
+   "WORLDS" 9
+   "MSI" 10})
+
+(defn get-league
+  [league]
+  (if (contains? (set (keys leagues)) league)
+    (str "http://api.lolesports.com/api/v1/leagues/" (leagues league))
+    (println (str "Can't get league info about " league ".  Valid leagues are " (keys leagues) "."))))
+
+(defn get-league-schedule
+  [league]
+  (if (contains? (set (keys leagues)) league)
+    (str "http://api.lolesports.com/api/v1/scheduleItems?leagueId=" (leagues league))
+    (println (str "Can't get schedule for " league ". Valid leagues are " (keys leagues) "."))))
+
+(defn extract-matches
+  [data start-date end-date]
+  (let [parsed-start (.parse js/Date start-date)
+        parsed-end (.parse js/Date end-date)]
+    (->> data
+         (filter
+          (fn item-in-date-range [{time :scheduledTime :as item}]
+            (let [parsed-date (.parse js/Date time)]
+              (and (<= parsed-date parsed-end)
+                   (>= parsed-date parsed-start)))))
+         (map
+          (fn format-match [{time :scheduledTime
+                             match :match
+                             bracket :bracket
+                             tournament :tournament}]
+            {:time time
+             :match match
+             :bracket bracket
+             :tournament tournament})))))
+
+(defn detail-matches
+  [detail-data sorted-matches]
+  (let [tournament-id (:tournament (first sorted-matches))
+        matching-tourneys (filter
+                           #(= tournament-id (:id %))
+                           (:highlanderTournaments detail-data))
+        current-tourney (first matching-tourneys)
+        brackets (:brackets current-tourney)]
+    (doseq [{match :match bracket :bracket time :time} sorted-matches]
+      (let [match-id (keyword match)
+            bracket-id (keyword bracket)
+            detail (bracket-id brackets)
+            match (match-id (:matches detail))]
+        (println time " - " (:name detail) " - " (:name match))))))
+
+(defn upcoming-matches
+  "Digs out the season, start date, and end date from a response to a
+   GET on the lolesports/scheduleItems API endpoint."
+  [data league-name]
+  (let [tournies (filter #(:published %) (:highlanderTournaments data))
+        {:keys [description startDate endDate] :as current-tourney} (last tournies)
+        matches (extract-matches (:scheduleItems data) startDate endDate)
+        sorted-matches (into (sorted-set-by time-<) matches)
+        league-detail-url (get-league league-name)]
+    (println (str "The current season is " description ".  It begins " startDate " and ends " endDate "."))
+    (interpret-data league-detail-url #(detail-matches (parse-json %) sorted-matches))))
 
 ;; cljs
 (defn -main
   [& args]
   (if (empty? args)
     (println "Provide a league to check:" (keys leagues))
-    (if-let [league-url (get-league (upper-case (first args)))]
-      (parse-response league-url))))
+    (let [league-name (upper-case (first args))]
+      (if-let [league-schedule-url (get-league-schedule league-name)]
+        (interpret-data league-schedule-url #(upcoming-matches (parse-json %) league-name))))))
 
 (set! *main-cli-fn* -main)
-
-;; lumo
-(comment
-  (if (empty? *command-line-args*)
-    (println "Provide a league to check:" (keys leagues))
-    (if-let [league-url (get-league (upper-case (first *command-line-args*)))]
-      (parse-response league-url))))

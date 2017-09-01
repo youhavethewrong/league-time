45a09f923e43bebedf05f7ab475227b267a59ccd
ESC <youhavethewrong@gmail.com>
2017-09-01 10:49:09 -0500

    No need for these scripts anymore.

--- scripts/build.cljs
+++
@@ -1,7 +0,0 @@
-(require '[lumo.build.api :as b])
-
-(b/build "src"
-         {:main 'league-time.core
-          :output-to "main.js"
-          :optimizations :advanced
-          :target :nodejs})

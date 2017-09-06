d877efdcc3ebf160f48d24153aa477c765ed0867
ESC <youhavethewrong@gmail.com>
2017-09-06 16:16:41 -0500

    Use pkg and lein-npm to build a linux binary.

--- project.clj
+++ project.clj
@@ -5,9 +5,13 @@
   :profiles {:dev
              {:dependencies [[com.cemerick/piggieback "0.2.2"]
                              [org.clojure/tools.nrepl "0.2.13"]]
-              :plugins [[lein-cljsbuild "1.1.7"]]
+              :plugins [[lein-cljsbuild "1.1.7"]
+                        [lein-npm "0.6.2"]]
               :source-paths ["src" "dev"]
               :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+  :npm {:devDependencies [pkg "4.2.4"]
+        :package {:scripts {:bin "pkg league-time.js -t node8-linux-x64"}}}
 
   :clean-targets [[:cljsbuild :builds 0 :compiler :output-to]
                   :target-path
